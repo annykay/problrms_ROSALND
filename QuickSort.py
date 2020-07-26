e5fa0eda88a53783579f684343257727e7937846
Анна <52636876+annykay@users.noreply.github.com>
2020-07-26 17:44:41 +0300

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -4,9 +4,15 @@
 # In[ ]:
 
 
-def qsort(massiv):
+def quick_sort(massiv):
     """ Функция быстрой сортировки. Сортирует массив по возрастанию"""
     if len(massiv)>0:
-        return qsort([i for i in massiv if i>massiv[0]])+[i for i in massiv if i == massiv[0]]+qsort([i for i in massiv  if i<massiv[0]])
-    return []
+        return quick_sort([i for i in massiv if i>massiv[0]])+[i for i in massiv if i == massiv[0]]+quick_sort([i for i in massiv  if i<massiv[0]])
+    else:
+        return []
+
+if __name__ == "__main__":
+    print('Enter the massiv')
+    tosort = [int(i) for i in input().split()]
+    print(' '.join(map(str, quick_sort(tosort))))
 
